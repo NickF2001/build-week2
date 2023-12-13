b9bb172ae163d9be98e20e2e58c3caa1cfb88914
NickF2001 <nicolefilippi01@gmail.com>
2023-12-13 17:54:26 +0100

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,56 @@
-const urlAlbum = 'https://striveschool-api.herokuapp.com/api/deezer/album/'
+const urlAlbum = 'https://striveschool-api.herokuapp.com/api/deezer/search?q='
+/*
 const arrayId = [
-    75621062,
-    443350855,
-    445994175,
-    185484062,
-    103879,
-    303196427
+    'Bellofigo',
+    'Ligabue',
+    'Queen',
+    'Beethoven',
+    'Bustarhymes',
+    'Pulcinopio'
 ]
+*/
 
-arrayId.forEach((el)=> {
-    fetch(urlAlbum + el)
-    .then(response => response.json())
-    .then(data => console.log(data))
+let artist = 'ligabue';
+/*
+arrayId.forEach((element)=> {
+        fetch(urlAlbum + arrayId.indexOf(element))
+        .then(response => response.json())
+        .then(data => {
+             console.log(data);
+             populateHomePage(data);
+        }
+        )
 })
+*/
+fetch(urlAlbum + artist)
+        .then(response => response.json())
+        .then(data => {
+             console.log(data);
+             populateHomePage(data);
+        })
+
+function populateHomePage(data) {
+
+        console.log(data[0]);
+        
+        const cards = document.getElementById('cards');
+        cards.innerHTML = '';
+        const cardDiv = document.createElement('div')
+        cardDiv.classList.add('card')
+        cardDiv.innerHTML += `
+        <img src="${data.picture_medium}" alt="">
+        <h3>${data.title}</h3>
+        <p>${data.artist.name}</p>
+        `
+        cards.appendChild(cardDiv);
+}
+/*
+<div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+*/
