c5fe581ebecbc4436bcc106ee3085731faa9ecdf
DraghettiGiacomo <jack.draghetti2002@gmail.com>
2023-12-18 23:52:06 +0100

    eliminato sfondo rosso, aggiunto offcanvas a tutte le pagine ma in quella degli artisti non funziona,

--- assets/js/script.js
+++ assets/js/script.js
@@ -57,7 +57,7 @@ function createAlbumTop(data){
             <small class="btn btn-sm btn-dark text-secondary ">NASCONDI ANNUNCI</small>
           </div>
           <h3 class="card-title">${data[numCasualDellArray].title}</h3>
-          <p class="card-text"><small>${data[numCasualDellArray].artist.name}</small></p>
+          <a href="artist-page.html?id=${data[numCasualDellArray].artist.id}"><p class="card-text"><small>${data[numCasualDellArray].artist.name}</small></p></a>
           <p class="card-text">Ascolta il nuovo singolo di ${data[numCasualDellArray].artist.name}</p>
           <div class="d-flex align-items-center gap-3">
             <a href="#" class="btn btn-success text-black rounded-pill btn-lg">Play</a>
@@ -89,10 +89,10 @@ function createAlbumPlaylist(data){
     <div class="card mb-3 bg-dark" style="max-width: 540px;">
     <div class="row g-0">
       <a class="row g-0" href="./album-page.html?id=${data.id}">
-        <div class="col-4">
+        <div class="col-3 col-sm-4">
           <img class="img-fluid" src="${data.cover}" class="card-img-top" alt="${data.artist.name}">
         </div>
-        <div class="col-6">
+        <div class="col-3 col-sm-6">
           <div class="card-body h-100 d-flex align-items-center">
             <h5 class="card-title text-light" style="white-space: nowrap; text-overflow: ellipsis; overflow: hidden;">${data.title}</h5>
           </div>
